e2daca4d7375e0af1ec362988f3d1d1c4cb94684
GitHub Action <action@github.com>
2025-04-17 15:06:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/hahaoshui/asimpleagent/MyBundle.kt
+++ src/main/kotlin/com/github/hahaoshui/asimpleagent/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hahaoshui.asimpleagent
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hahaoshui/asimpleagent/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/hahaoshui/asimpleagent/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.hahaoshui.asimpleagent.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.hahaoshui.asimpleagent.MyBundle
+import com.github.hahaoshui.asimpleagent.services.MyProjectService
 import javax.swing.JButton
 
 
